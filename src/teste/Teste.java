c7cfcc244c4f265c0bf7fa653c5df8f6bc379f5c
pvhal <pvhal@192.168.1.8>
2018-11-20 21:57:42 -0200

    commit versão 1.1

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -17,7 +17,7 @@ public class Teste {
     public static void main(String[] args) {
         
 // TODO code application logic here
-        System.out.println("teste te");
+        System.out.println("teste te ole");
     }
     
 }
