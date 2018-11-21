b66c0231040fde726d0a1e02d409ae687334d1fa
LuizaLemos <43822003+LuizaLemos@users.noreply.github.com>
2018-11-20 22:20:26 -0200

    teste1.2
    alteração 1.2

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -17,7 +17,7 @@ public class Teste {
     public static void main(String[] args) {
         
 // TODO code application logic here
-        System.out.println("teste te ole");
+        System.out.println("teste te ole ola turu bom!");
     }
     
 }
